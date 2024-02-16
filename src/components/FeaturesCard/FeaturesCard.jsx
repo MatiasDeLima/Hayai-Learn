11b394e317ab0c9f3c0c5d85ce1e268ae411d34b
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2024-02-16 11:23:45 -0300

    feat() create features section

---
+++ src/components/FeaturesCard/FeaturesCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./FeaturesCard.css";
+
+export const FeaturesCard = (props) => {
+  return (
+    <>
+      <svg className="features-mask">
+        <clipPath id="features-clip">
+          <path
+            d="M163.5 38.0936C193.901 38.0936 190.81 0 226.364 0H274.236C292.705 0 301.939 0 308.994 3.59088C315.199 6.7495 320.244 11.7896 323.406 17.9887C327 25.0362 327 34.2619 327 52.7133V342.287C327 360.738 327 369.964 323.406 377.011C320.244 383.21 315.199 388.251 308.994 391.409C301.939 395 292.705 395 274.236 395H52.7645C34.2952 395 25.0605 395 18.0062 391.409C11.801 388.251 6.75605 383.21 3.59436 377.011C2.38419e-07 369.964 0 360.738 0 342.287V52.7133C0 34.2619 2.38419e-07 25.0362 3.59436 17.9887C6.75605 11.7896 11.801 6.7495 18.0062 3.59088C25.0605 0 34.2952 0 52.7644 0H101.667C133.614 0 133.099 38.0936 163.5 38.0936Z"
+            fill="white"
+          />
+        </clipPath>
+      </svg> 
+
+        <article className="features__card">
+            <div className="fetures__card-circle">
+            <i className="ri-global-line"></i>
+            </div>
+
+            <div className="features__card-bg">
+            <div className="features__card-data">
+                <img src={props.image} alt="" />
+
+                <h3 className="features__card-title">
+                {props.title}
+                </h3>
+
+                <p className="features__card-description">
+                {props.content}
+                </p>
+            </div>
+
+            <button className="button features__button">
+                Learn More
+                <div className="button__icon">
+                <i className="ri-arrow-right-up-line"></i>
+                </div>
+            </button>
+            </div>
+        </article>
+    </>
+  );
+};
--- src/sections/Features/Features.jsx
+++ src/sections/Features/Features.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import "./Features.css";
+import { FeaturesCard } from '../../components/FeaturesCard/FeaturesCard.jsx';
+import { features } from '../../assets/data/index.js';
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from 'swiper/modules';
+import "swiper/css";
+import 'swiper/css/pagination';
 
 const Features = () => {
   return (
-    <section>Features</section>
+    <section className="features section">
+      <h2 className="features__title">
+        <span className="text__line">Exciting</span> and Robust<br /> <span className="orange__text-color">Features</span> That Set Our<br /> <span className="orange__text-color">Product</span> Apart.
+      </h2>
+
+      <div className="features__shadow"></div>
+
+      <div className="features__container container">
+        <Swiper
+          spaceBetween={24}
+          slidesPerView={1}
+          centeredSlides={true}
+          grabCursor={true}
+          pagination={true}
+          modules={[Pagination]}
+          className="feature-swiper"
+        >
+          {features.map((feature, index) => (
+            <SwiperSlide key={feature.id}>
+              <FeaturesCard 
+                id={feature.id}
+                key={feature.id}
+                image={feature.img}
+                title={feature.title}
+                content={feature.content}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
   )
 }
 
-export default Features
+export default Features;

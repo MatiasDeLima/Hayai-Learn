b657bd80c69a5f56352f437efcf64a21d594ac3e
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2024-02-27 08:46:19 -0300

    update

--- src/sections/FAQ/FAQ.jsx
+++ src/sections/FAQ/FAQ.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 import "./FAQ.css";
 
 const FAQ = () => {
   return (
-    <section>FAQ</section>
-  )
-}
+    <section className="faq section">
+      <h2 className="faq__title">Frequently Ask <br/> 
+      <span className="orange__text-color">Questions</span>
+      </h2>
 
-export default FAQ
+      <div className="faq__container container grid"></div>
+    </section>
+  );
+};
+
+export default FAQ;
